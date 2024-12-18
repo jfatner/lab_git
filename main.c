b73ff21477da1b0fef57795df2bf1769668ee6b5
jfatner <255588@edu.p.lodz.pl>
2024-12-18 15:10:45 +0100

    nowa metoda policz(mnozenie)

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+int Policz (int a, int b) {
+    return a*b;
+}
 int main(void) {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu 2*3=: %d",Policz(2,3));
     return 0;
 }
